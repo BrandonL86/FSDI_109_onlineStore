329eff3dcdc12a8d34d94ca489326e991009b5d4
BrandonL86 <131834912+BrandonL86@users.noreply.github.com>
2023-07-15 09:21:10 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+import { React } from "react";
+import NavBar from './components/navBar';
+import Footer from './components/footer';
+import Catalog from "./pages/catalog";
+import Home from './pages/home';
+import About from './pages/About';
+import Cart from './pages/cart';
+import Admin from './pages/Admin';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// import QuantityPicker from './components/quantityPicker';
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+    <div className="App">
+        <NavBar/>
+
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/Admin' element={<Admin />} />
+        </Routes>
+        {/* <Home />
+        <Catalog/> */}
+        {/* <QuantityPicker/> */}
+        <br></br>
+        <Footer/>
+    </div>
+    
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
